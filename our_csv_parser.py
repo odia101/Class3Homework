53b296c6ae5cf2e0d0a7026c3a62271b9663a256
odia101 <odia.akioya@mail.com>
2019-02-09 05:53:17 -0500

    Casting value to appropriate data type

--- our_csv_parser.py
+++ our_csv_parser.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import os
-import csv
+#import numpy
+#import csv
 myfilename = "housing.data.txt"
 
 # if os.path.isfile(myfilename):
@@ -16,22 +17,26 @@ with open(myfilename, 'r') as file_handle:
 		line_clean = line.replace('   ', ' ').replace('  ', ' ')
 		line_clean = line_clean.strip()
 		values = line_clean.split(' ')
+		caster = []
 		print(values)
 		for value in values:
+			if '.' in value:
+				caster.append(float(value))
+			else:
+				caster.append(int(value))
+		print(caster)
+
             # for homework:
             # identify what type of data each value is, and cast it
             # to the appropriate type, then print the new, properly-typed
             # list to the screen.
             # I.e. ['0.04741', '0.00', '11.930', '0', '0.5730', '6.0300', '80.80', '2.5050', '1', '273.0', '21.00', '396.90', '7.88', '11.90']
             # becomes: [0.04741, 0.0, 11.93, 0, 0.573, '6.03, 80.8, 2.505, 1, 273.0, 21.0, 396.90, 7.88, 11.90]
- 			print(value[0])
 
 print('finished!')
 
-with open(myfilename, 'r') as csv_file:
-	csv_reader=csv.reader(csv_file)
-	for line in csv_reader:
-		print(line[0])
+
+
 
 
 
